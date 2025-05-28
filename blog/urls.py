9e78af656489e2f8bcc6bd0ea19e468a31657be9
DermisZabala <dermiszabala@gmail.com>
2025-05-27 23:47:32 -0400

    Implementadas vistas de lista y detalle de posts, con sus respectivas plantillas: post_list y post_detail

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog' # Namespace para las URLs de esta app
+
+# Aqui definimos las URLs especificas del blog
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('blog/<int:pk>/', views.post_detail, name='post_detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post # importando el modelo Post
+from django.utils import timezone # importando timezone
 
 # Create your views here.
+
+
+def post_list(request):
+    # obtener todos los posts que estan publicados y cuya fecha de publicacion no es futura
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-create_date')
+
+    # el contexto son los datos que pasamos a la plantilla
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    # obtener el post especifico por su pk (Primary key)
+    # si el post no existe o no esta publicado, devuelve un error 404 (Not Found)
+    
+    post = get_object_or_404(Post, pk=pk) # usando el pk recibido para buscar el post
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'blog/post_detail.html', context)
