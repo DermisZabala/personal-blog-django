77fd1e265a76327ec839a08084f2e19e30877b38
DermisZabala <dermiszabala@gmail.com>
2025-06-06 21:19:52 -0400

    Mejorando el CSS con flexblox.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog' # Namespace para las URLs de esta app
 # Aqui definimos las URLs especificas del blog
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('blog/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog-<int:pk>/', views.post_detail, name='post_detail'),
 ]
